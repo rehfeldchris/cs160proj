bbb5d98802f8ec27b35869dc0f135c8b82128450
rehfeldchris <rehfeldchris@gmail.com>
2013-05-18 12:50:57 -0700

    add comments

--- addComment.php
+++ addComment.php
@@ -1,20 +1,32 @@
 <?php
 
+/*
+ * This file receives the post request to add a new comment. 
+ * 
+ * If the request wasn't sent with ajax, it redirects the user back to the page it assumed the browser came from.
+ * 
+ * @author Chris Rehfeld
+ */
+
+
+
 require_once 'connection.php';
 
 ignore_user_abort(true);
 
+//make sure data sent
 if (!isset($_POST['message'], $_POST['courseId'])) {
     header('HTTP/1.1 500 Internal Server Error', true, 500);
     exit;
 }
 
+//check if request was via ajax. if not, redirect them(but this script will still record their comment)
 if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest') {
     header("Location: courseDetail.php?courseId={$_POST['courseId']}");
 }
 
 
-
+//make sure data sent
 if (!strlen($_POST['message']) || !ctype_digit($_POST['courseId'])) {
     header('HTTP/1.1 500 Internal Server Error', true, 500);
     exit;
@@ -24,6 +36,7 @@ if (get_magic_quotes_gpc()) {
 	$_POST['message'] = stripslashes($_POST['message']);
 }
 
+//add the comment
 $sql = "insert into comments (course_data_id, comment, when_posted) values (?, ?, now())";
 $stmt = $dbc->prepare($sql);
 if (!$stmt) {
@@ -37,3 +50,4 @@ if (!$stmt->execute()) {
     exit;
 }
 
+//send no status header if we reach here, assume "200 ok" will be sent by server default
